97f58d5ef4ac8698ca2bc5ad7e22d1ab059fe5e3
nsavinda <nirmalsavinda29@gmail.com>
2024-06-20 14:05:18 +0530

    feat:add reg  routes

--- src/api/services/auth.ts
+++ src/api/services/auth.ts
@@ -1,7 +1,24 @@
 import api from '../config';
 import { AxiosResponse } from 'axios';
-import { LoginCredentials, AuthResponse } from '../types';
+import { LoginCredentials, StudentRegister, EducatorRegister,OrganizationRegister, AuthResponse } from '../types';
 
 export const login = (credentials: LoginCredentials): Promise<AxiosResponse<AuthResponse>> => {
     return api.post<AuthResponse>('/auth/login', credentials);
 };
+
+export const registerStudent = (student: StudentRegister): Promise<AxiosResponse<AuthResponse>> => {
+    return api.post<AuthResponse>('/auth/register/student', student);
+};
+
+export const registerEducator = (educator: EducatorRegister): Promise<AxiosResponse<AuthResponse>> => {
+    return api.post<AuthResponse>('/auth/register/educator', educator);
+};
+
+export const registerOrganization = (organization: OrganizationRegister): Promise<AxiosResponse<AuthResponse>> => {
+    return api.post<AuthResponse>('/auth/register/organization', organization);
+};
+
+
+
+
+// export const registerStudent 
