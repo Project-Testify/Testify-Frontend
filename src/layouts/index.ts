d1771a083d5d29189b46488d220d2772eebf9943
nsavinda <nirmalsavinda29@gmail.com>
2024-06-21 14:38:07 +0530

    remove unused files

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,4 +1,2 @@
-export { AppLayout } from './app';
 export { DashboardLayout } from './dashboard/DashboardLayout';
-export { GuestLayout } from './guest/Guest.tsx';
 export { UserAccountLayout } from './userAccount';
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,4 @@
 
-import type {
-  LearningCourses,
-  RecommendedCourses,
-  Exam,
-  CommunityGroup,
-} from './learnings.ts';
-
-import type { Employee } from './employee';
-import type { Faq } from './faq';
 
 import type { Exams } from './exams';
 
@@ -15,12 +6,6 @@ import type { Exams } from './exams';
 export type {
   Exams,
 
-  LearningCourses,
-  RecommendedCourses,
-  Exam,
-  CommunityGroup,
 
-  Employee,
-  Faq,
 
 };
