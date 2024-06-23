943d014f0e4069f99cc3b22c496d39136f4350e6
Nirmal Savinda <nirmalsavinda29@gmail.com>
2024-06-23 20:42:27 +0530

    fix:cors (#10)

--- src/api/config.ts
+++ src/api/config.ts
@@ -18,9 +18,17 @@ api.interceptors.request.use(
         if (token) {
             config.headers['Authorization'] = `Bearer ${token}`;
         }
+
+        config.headers['Access-Control-Allow-Origin'] = '*';
+        config.headers['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,PATCH,OPTIONS';
+
+
+        
+
         return config;
     },
     error => Promise.reject(error)
 );
+
 
 export default api;
