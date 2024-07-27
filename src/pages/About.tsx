a0a58ef92507c1d2d9189372fb376c5e77f1b5b6
jananga <kavinduwijewardhana1999@gmail.com>
2024-07-27 13:08:20 +0530

    Add Learn More and Show Less button

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,16 @@
 import { Typography, Row, Col, Button} from 'antd';
 import { Container } from '../components';
+import { useState } from 'react';
 const { Title} = Typography;
 
 
 const { Paragraph } = Typography;
 export const About = () => {
-  
+  const [showMore, setShowMore] = useState(false);
+
+  const handleLearnMore = () => {
+    setShowMore(!showMore);
+  };
 
   return (
 
@@ -40,14 +45,25 @@ export const About = () => {
 
     <Row justify="center" style={{ marginTop: '10px' }}>
         <Col span={12} >
-          <Title level={2} style={{ color: 'black', fontWeight: 'bold',marginTop:'70px' }}>
+          <Title level={2} style={{ color: 'black', fontWeight: 'bold',marginTop:'80px' }}>
             ABOUT US
           </Title>
-          <Paragraph style={{marginRight:'40px'}}>
+          <Paragraph style={{marginRight:'40px', marginTop:'25px'}}>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed mauris commodo erat rutrum tincidunt. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla odio sed vehicula. printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the.
           </Paragraph>
-          <Button type="primary" size="large">
-            Learn More
+          {showMore && (
+            <Paragraph style={{marginRight:'40px'}}>
+              Contrary to popular belief, Lorem Ipsum is not simply random text.
+              It has roots in a piece of classical Latin literature from 45 BC,
+              making it over 2000 years old. Richard McClintock, a Latin
+              professor at Hampden-Sydney College in Virginia, looked up one of
+              the more obscure Latin words, consectetur, from a Lorem Ipsum
+              passage, and going through the cites of the word in classical
+              literature, discovered the undoubtable source.
+            </Paragraph>
+          )}
+          <Button type="primary" size="large" onClick={handleLearnMore}>
+            {showMore ? 'Show Less' : 'Learn More'}
           </Button>
         </Col>
         <Col span={12} style={{marginTop:'-50px'}}>
