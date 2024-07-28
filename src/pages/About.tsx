fe9a00b6b4875f7a7eafeee83799fdddefa53079
jananga <kavinduwijewardhana1999@gmail.com>
2024-07-28 10:07:00 +0530

    Added Navbar to 'About Page'

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,19 @@
-import { Typography, Row, Col, Button} from 'antd';
-import { Container } from '../components';
+import { Typography, Row, Col, Button, Menu} from 'antd';
+import { Container, Logo } from '../components';
 import { useState } from 'react';
-const { Title} = Typography;
+import { Link } from 'react-router-dom';
+import { PATH_AUTH } from '../constants';
+import { Header } from 'antd/es/layout/layout';
 
+const { Title, Paragraph} = Typography;
 
-const { Paragraph } = Typography;
+const menuItems = [
+  { label: <Link to="/">Home</Link>, key: 'home' },
+  { label: <Link to="#">about</Link>, key: 'corporate' },
+  { label: <Link to="#">contact us</Link>, key: 'profile' },
+  { label: <Link to={PATH_AUTH.signin}><Button type='primary' style={{borderRadius:'50px'}}>Login</Button></Link>, key: 'login' },
+];
+
 export const About = () => {
   const [showMore, setShowMore] = useState(false);
 
@@ -13,6 +22,14 @@ export const About = () => {
   };
 
   return (
+
+    <>
+    <Header style={{ display: 'flex',flexDirection:'row', backgroundColor:'white', alignItems:'center'}}>
+        <div className='logo' style={{marginRight:'auto'}}>
+          <Logo color='black'/>
+        </div>
+        <Menu mode='horizontal' items={menuItems} style={{flex:'1', justifyContent:'flex-end', fontSize:'16px'}}/>
+      </Header>
 
     <Container>
     <Row justify="center" style={{ marginTop: '20px'}}>
@@ -70,6 +87,7 @@ export const About = () => {
       </Row>
 
     </Container>
+    </>
 
     
   );
