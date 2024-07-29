2e552f9a1a02a4de1aedcf04d36c35b83bdf27e2
nsavinda <nirmalsavinda29@gmail.com>
2024-07-29 12:35:30 +0530

    fix:logout

--- src/layouts/dashboard/DashboardLayout.tsx
+++ src/layouts/dashboard/DashboardLayout.tsx
@@ -1,10 +1,15 @@
 import { AppLayout } from '../app';
 import { Outlet } from 'react-router-dom';
+import { AuthProvider } from '../../hooks/useAuth.tsx';
+
 
 export const DashboardLayout = () => {
   return (
+    <AuthProvider>
     <AppLayout>
+    
       <Outlet />
     </AppLayout>
+    </AuthProvider>
   );
 };
