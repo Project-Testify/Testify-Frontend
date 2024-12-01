462ac48e62125b72ee786d0ad913f875c0805621
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2024-12-01 19:59:11 +0530

    fix

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -42,8 +42,7 @@ import {
   Moderating,
   ModeratingExam,
   OrgAdminCompleteExamPage,
-  OrgAdminViewCompleteExamPage,
-  Proctoring
+  OrgAdminViewCompleteExamPage
 } from '../pages';
 
 import {
