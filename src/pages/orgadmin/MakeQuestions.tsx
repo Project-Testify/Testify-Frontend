bf6d72e41a1468a968f1b322c8d88acefc305798
nsavinda <nirmalsavinda29@gmail.com>
2024-12-02 20:59:39 +0530

    add missing button

--- src/pages/orgadmin/MakeQuestions.tsx
+++ src/pages/orgadmin/MakeQuestions.tsx
@@ -343,8 +343,15 @@ const MakeQuestions = () => {
           title="Upload Content"
           onOk={handleUpload}
           onCancel={handleCancel}
-          footer={null}
-        >
+          footer={[
+            <Button key="cancel" onClick={handleCancel}>
+              Cancel
+            </Button>,
+            <Button key="submit" type="primary" onClick={handleUpload}>
+              Submit
+            </Button>,
+          ]}
+                  >
           <Form form={form}>
             <Form.Item name="upload" label="Upload">
               <Upload
@@ -355,7 +362,9 @@ const MakeQuestions = () => {
               >
                 <Button icon={<UploadOutlined />}>Click to Upload</Button>
               </Upload>
+              
             </Form.Item>
+            
           </Form>
         </Modal>
 
