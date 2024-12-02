570c2aea5e11e93d60891048afb087284832025e
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2024-12-02 23:21:34 +0530

    bug fix and exam setter deletion

--- src/api/services/ExamSetter.ts
+++ src/api/services/ExamSetter.ts
@@ -1,15 +1,34 @@
-import { AxiosResponse } from "axios";
-import api from "../config";
-import { GenericAddOrUpdateResponse, OrganizationResponse } from "../types";
+import { AxiosResponse } from 'axios';
+import api from '../config';
+import { GenericAddOrUpdateResponse, OrganizationResponse } from '../types';
 
-export const getExamSetterOrganizations = (setterId:number):Promise<AxiosResponse<OrganizationResponse[]>> => {
-    return api.get<OrganizationResponse[]>(`/exam-setter/${setterId}/getOrganizations`);
-}
+export const getExamSetterOrganizations = (
+  setterId: number
+): Promise<AxiosResponse<OrganizationResponse[]>> => {
+  return api.get<OrganizationResponse[]>(
+    `/exam-setter/${setterId}/getOrganizations`
+  );
+};
 
-export const checkSetterRegistration = (token:string): Promise<AxiosResponse<number>>=>{
-    return api.get<number>(`/exam-setter/${token}/checkSetterRegistration`);
-}
+export const checkSetterRegistration = (
+  token: string
+): Promise<AxiosResponse<number>> => {
+  return api.get<number>(`/exam-setter/${token}/checkSetterRegistration`);
+};
 
-export const addExamSetter = (token: string): Promise<AxiosResponse<GenericAddOrUpdateResponse>> => {
-    return api.post<GenericAddOrUpdateResponse>(`/exam-setter/${token}/addSetterToOrganization`);
+export const addExamSetter = (
+  token: string
+): Promise<AxiosResponse<GenericAddOrUpdateResponse>> => {
+  return api.post<GenericAddOrUpdateResponse>(
+    `/exam-setter/${token}/addSetterToOrganization`
+  );
+};
+
+export const removeExamSetterFromOrganization = (
+  setterId: number,
+  orgId: number
+): Promise<AxiosResponse<GenericAddOrUpdateResponse>> => {
+  return api.put<GenericAddOrUpdateResponse>(
+    `/exam-setter/${setterId}/${orgId}/deleteSetter`
+  );
 };
