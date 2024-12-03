67d55ebe3f777f5aa2087027dfff75716972d628
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2024-12-03 23:45:37 +0530

    remove exm setter function fix

--- src/api/services/ExamSetter.ts
+++ src/api/services/ExamSetter.ts
@@ -1,7 +1,12 @@
-import { AxiosResponse } from "axios";
-import api from "../config";
-import {  ModerateExamResponse } from "../types";
-import { CandidateResponse, ExamResponse, GenericAddOrUpdateResponse, OrganizationResponse } from "../types";
+import { AxiosResponse } from 'axios';
+import api from '../config';
+import { ModerateExamResponse } from '../types';
+import {
+  CandidateResponse,
+  ExamResponse,
+  GenericAddOrUpdateResponse,
+  OrganizationResponse,
+} from '../types';
 
 export const getExamSetterOrganizations = (
   setterId: number
@@ -17,23 +22,52 @@ export const checkSetterRegistration = (
   return api.get<number>(`/exam-setter/${token}/checkSetterRegistration`);
 };
 
-export const addExamSetter = (token: string): Promise<AxiosResponse<GenericAddOrUpdateResponse>> => {
-    return api.post<GenericAddOrUpdateResponse>(`/exam-setter/${token}/addSetterToOrganization`);
+export const addExamSetter = (
+  token: string
+): Promise<AxiosResponse<GenericAddOrUpdateResponse>> => {
+  return api.post<GenericAddOrUpdateResponse>(
+    `/exam-setter/${token}/addSetterToOrganization`
+  );
 };
 
 export const getModeratingExams = (
-    examSetterId: number
+  examSetterId: number
 ): Promise<AxiosResponse<ModerateExamResponse[] | null>> => {
-    return api.get<ModerateExamResponse[] | null>(`/exam-setter/${examSetterId}/moderating-exams`);
+  return api.get<ModerateExamResponse[] | null>(
+    `/exam-setter/${examSetterId}/moderating-exams`
+  );
 };
-export const getProctoringExams = (proctorId:number, organizationId:number): Promise<AxiosResponse<ExamResponse[]>> => {
-    return api.get<ExamResponse[]>(`/exam-setter/proctor/${proctorId}/${organizationId}`);
-}
+export const getProctoringExams = (
+  proctorId: number,
+  organizationId: number
+): Promise<AxiosResponse<ExamResponse[]>> => {
+  return api.get<ExamResponse[]>(
+    `/exam-setter/proctor/${proctorId}/${organizationId}`
+  );
+};
 
-export const getProctoringCandidates = (examId:number): Promise<AxiosResponse<CandidateResponse[]>> => {
-    return api.get<CandidateResponse[]>(`/exam-setter/${examId}/candidates`);
-}
+export const getProctoringCandidates = (
+  examId: number
+): Promise<AxiosResponse<CandidateResponse[]>> => {
+  return api.get<CandidateResponse[]>(`/exam-setter/${examId}/candidates`);
+};
 
-export const addProctorComment = (candidateId:number, examId:number, content:string): Promise<AxiosResponse<any>> => {
-    return api.post<any>(`/exam-setter/${candidateId}/${examId}/proctorComments`, content);
-}
+export const addProctorComment = (
+  candidateId: number,
+  examId: number,
+  content: string
+): Promise<AxiosResponse<any>> => {
+  return api.post<any>(
+    `/exam-setter/${candidateId}/${examId}/proctorComments`,
+    content
+  );
+};
+
+export const removeExamSetterFromOrganization = (
+  setterId: number,
+  orgId: number
+): Promise<AxiosResponse<GenericAddOrUpdateResponse>> => {
+  return api.put<GenericAddOrUpdateResponse>(
+    `/exam-setter/${setterId}/${orgId}/deleteSetter`
+  );
+};
